021c5d4f8dcf46542a1e44eef743a8d9fa54b4be
Blaine Love <blainelove94@gmail.com>
2021-03-22 19:19:55 -0400

    commit tree f5c5a8f37b18bfebd7e69185b245a0c44be5471e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,9 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function Eyes() {
+      console.log("Hey! Eyes on me!");
+    }
+  
+    return <button onClick={Eyes}>Eyes on me</button>;
+  } 
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,28 @@
 // Code Keypad Component Here
+
+function Keypad(){
+    function handleClick(){
+        console.log(`${number}`)
+    }
+
+    return <div>
+      <break><button onClick={() => handleClick(1)}>1</button>
+      <button onClick={() => handleClick(2)}>2</button>
+      <break><button onClick={() => handleClick(3)}>3</button></break>
+      <button onClick={() => handleClick(5)}>4</button>
+      <button onClick={() => handleClick(5)}>5</button>
+      <button onClick={() => handleClick(6)}>6</button></break>
+      <button onClick={() => handleClick(7)}>7</button>
+      <button onClick={() => handleClick(8)}>8</button>
+      <button onClick={() => handleClick(9)}>9</button>
+    
+
+    
+    </div>
+
+
+}
+
+
+
+export default Keypad;
